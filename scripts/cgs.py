0d7479a0d5d19afdaa32d5f5c5a2bc3c8c354a76
hagrusa <hagrusa@astro.umd.edu>
2018-12-02 13:13:29 -0500

    cgs

---
+++ scripts/cgs.py
@@ -0,0 +1,87 @@
+# CGS constants
+#mostly from NIST
+#some still need updateing
+c = 2.99792458e10            # speed of light, cm/s
+h = 6.626070040e-27        # Planck constant, erg*s
+hbar = 1.05e-27     # reduced Planck constant, erg*s
+G = 6.67408e-8         # gravitational constant, cm^3/g/s^2
+e = 4.8e-10         # charge of electron, esu
+me = 9.10938356e-28       # mass of electron, g
+mp = 1.672621898e-24    # mass of proton, g
+mn = 1.67493e-24    # mass of neutron, g
+mh = 1.67492e-24    # mass of hydrogen, g
+amu = 1.660539040e-24     # atomic mass unit, g
+Na = 6.022e23       # Avagadro's number, --
+a0 = 5.29e-9        # Bohr radius, cm
+k = 1.38064852e-16       # Boltzmann constant, erg/K
+eV = 1.6022e12      # electronvolt, erg
+a = 7.565e-15       # radiation density constant, erg/cm^3/K^4
+sigma = 5.670367e-5    # Stefan-Boltzmann constant, erg/cm^2/K^4/s
+alpha = 7.2973525664e-3    # fine structure constant, --
+R = 2.1799e-11      # Rydberg consant, erg
+Rgas = 8.31447e7	# gas constant
+AU = 1.495978707E13       # astronomical unit, cm
+pc = 3.086E18       # parsec, cm
+ly = 9.463e17       # lightyear, cm
+Msun = 1.988475415966536153133245005824e33   # mass of sun, g #digits to match benchmarking stuff
+Rsun = 6.96e10      # radius of sun, cm
+Lsun = 3.9e33       # luminosity of sun, erg/s
+Tsun = 5.78e3       # effective temperature of sun, K
+sigmaT = 6.6542e-25 #cm^2
+
+#sun-planet distances in cm
+#from https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+d_mercury = 57.9e11
+d_venus = 108.2e11
+d_earth = 149.6e11
+d_moon = 0.384e11 #(earth-moon distance)
+d_mars = 227.9e11
+d_jupiter = 778.6e11
+d_saturn = 1433.5e11
+d_uranus = 2872.5e11
+d_neptune = 4495.1e11
+d_pluto = 5906.4e11
+
+d_io = 421700e5 #cm from jupiter
+
+
+#planet masses
+M_mercury = 0.330e27
+M_venus = 4.87e27
+M_earth = 5.9723e27
+M_moon = 0.073e27
+M_mars = 0.642e27
+M_jupiter = 1898e27
+M_saturn = 568e27
+M_uranus = 86.8e27
+M_neptune = 102e27
+M_pluto = 0.0146e27
+
+#planet radii
+R_mercury = 2439.5e5
+R_venus = 	6052e5
+R_earth = 	6378e5
+R_moon = 	1737.5e5
+R_mars = 	3396e5
+R_jupiter = 71492e5
+R_saturn =	60268e5
+R_uranus = 	25559e5
+R_neptune =	24764e5
+R_pluto = 	1185e5
+
+
+#planet eccentricities
+e_mercury = 0.20564
+e_venus = 0.00678
+e_earth = 0.01671
+e_moon = 0.0549006 #earth-moon eccentricity
+e_mars = 0.09339
+e_jupiter = 0.0484
+e_saturn = 0.0539
+e_uranus = 0.04726
+e_neptune = 0.00859
+e_pluto = 0.24883
+e_io = 0.0041
+
+
+
